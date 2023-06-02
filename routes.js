bd4f6e9bc32ac1ea2a69c767e5aff187cbffa797
EC2 Default User <ec2-user@ip-172-31-16-97.ap-southeast-1.compute.internal>
2023-06-02 02:49:57 +0000

    first commit

---
+++ routes.js
@@ -0,0 +1,26 @@
+const routes = [
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+];
+
+module.exports = routes;
+
